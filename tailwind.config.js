6b3058ce580f7a2c4334d0a65dfbdfadbfb77968
Hasan Oruc <oruch379@gmail.com>
2024-02-23 21:28:47 +0300

    Added image, clock, and some links.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-black': '#1f2328',
+        'bg-white': '#f8f9fa',
+        'bg-gray': '#636c76',
+        'bg-light-gray': '#6e7781',
+        'bg-blue': '#0969da',
+        'bg-green': '#1f883d',
+        'bg-yellow': '#9a6700',
+        'bg-orange': '#bc4c00',
+        'bg-red': '#cf222e',
+        'bg-purple': '#8250df',
+        'bg-pink': '#bf3989',
+      },
+      fontFamily: {
+        'playfair': 'Playfair Display, serif'
+      }
+    },
   },
   plugins: [],
 }
